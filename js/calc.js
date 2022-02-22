1f893f97c411ee80879229ccf3e0d1442f6b1a09
viniTWL <vinigdc12@gmail.com>
2022-02-22 14:54:35 -0300

    script done
    Adicionando todas as funções

--- js/calc.js
+++ js/calc.js
@@ -1,23 +1,21 @@
 let checkbox = document.getElementById('toogle');
 let title = document.querySelector('.title')
-let resultBg = document.querySelector('.result')
+let resultBg = document.querySelector('.screen')
 let buttonsBg = document.querySelector('.buttons')
-let numBg = document.querySelectorAll('.num')
+let numBg = document.querySelectorAll('.num')               // Selecionando elementos para manipular o estilo
 let resetBg = document.querySelectorAll('.reset')
 let equalBg = document.querySelector('.equal')
-
-window.onload = function() {
-    darkMode()
-}
+let result = document.querySelector('.result')
 
 let darkMode = () => {
-    if (checkbox.checked == false){
 
+    if (checkbox.checked == false){ // Se o checkbox estiver false, light mode ativado, se não, dark mode ativado
         document.body.style.backgroundColor = 'hsl(0, 0%, 90%)';
         title.style.color = 'hsl(60, 10%, 19%)'
         resultBg.style.backgroundColor = 'hsl(0, 0%, 93%)';
         buttonsBg.style.backgroundColor = 'hsl(0, 5%, 81%)';
         equalBg.style.backgroundColor = 'hsl(25, 98%, 40%)';
+        result.style.color = 'hsl(60, 10%, 19%)'
 
         numBg.forEach(element => {
             element.style.backgroundColor = 'hsl(45, 7%, 89%)';
@@ -30,12 +28,14 @@ let darkMode = () => {
             element.style["boxShadow"] = '0px 3.5px 2px hsl(35, 11%, 61%)'
         });
     }
+
     else{
         document.body.style.backgroundColor = 'hsl(222, 26%, 31%)';
         title.style.color = 'white'
         resultBg.style.backgroundColor = 'hsl(224, 36%, 15%)';
         buttonsBg.style.backgroundColor = 'hsl(223, 31%, 20%)';
         equalBg.style.backgroundColor = 'hsl(6, 63%, 50%)';
+        result.style.color = 'white'
 
         numBg.forEach(element => {
             element.style.backgroundColor = 'hsl(225, 21%, 49%)';
@@ -49,4 +49,36 @@ let darkMode = () => {
             element.style.color = 'white';
         });
     }
+
+}
+
+window.onload = function() {
+    darkMode() // Ativa o light mode quando a página carrega
+}
+
+let insertNum = (number) => {
+    let result = document.querySelector('.result').innerText;
+    document.querySelector('.result').innerText = result + number; // insere um novo numero além daquele que já está na tela
+}
+
+let calc = () => {
+    let result = document.querySelector('.result').innerHTML;
+    if (result){
+       let total = (eval(result)) //faz o calculo de strings que estão em um mesmo elemento, no caso o <p>
+       document.querySelector('.result').innerText = total 
+    } 
+}
+
+let del = () => {
+    let result =  document.querySelector('.result').innerHTML
+    let delChar = result.slice(0, -1) // o slice pega como primeiro parametro o indice 0 do elemento, e o segundo parametro define a ordem
+                                        // que ele irá remover os indices, logo -1, começa pelo ultimo
+
+    document.querySelector('.result').innerText = delChar // insere na tela o valor novo com o ultimo elemento deletado
+}
+
+let reset = () => {
+    let result =  document.querySelector('.result').innerHTML
+    let delChar = result.slice(0, 0) // parecido com a função del(), mas deleta todos os elementos
+    document.querySelector('.result').innerText = delChar 
 }
